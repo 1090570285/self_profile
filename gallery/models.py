e074195b3311c4d034e3efda416979e2b0f381fa
1090570285 <1090570285@qq.com>
2020-06-23 13:21:44 +0800

    在管理员页面设置title，image
    设置了media的根目录，image文件路径

--- gallery/models.py
+++ gallery/models.py
@@ -4,4 +4,8 @@ from django.db import models
 
 
 class Gallery(models.Model):
-    description = models.CharField(max_length=100)
+    description = models.CharField(default='作品的描述', max_length=100)
+    image = models.ImageField(default='default.png', upload_to='images/')
+    title = models.CharField(default='作品的标题', max_length=20)
+    def __str__(self):
+        return self.title
