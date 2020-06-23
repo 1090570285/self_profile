4aaa2c0e3dd20f3fb5d6d3a16405dda113f6624b
1090570285 <1090570285@qq.com>
2020-06-23 17:14:19 +0800

    创建一个blogapp
    实现title/data/image/text等功能

--- self_profile/settings.py
+++ self_profile/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 自己的app
     'gallery.apps.GalleryConfig',
+    'blog',
 
 ]
 
